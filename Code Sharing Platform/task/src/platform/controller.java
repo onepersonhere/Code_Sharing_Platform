5061b976877c640e4d2218dbaf27edaab974a397
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2021-07-11 07:51:26 +0800

    parsed Json

--- Code Sharing Platform/task/src/platform/controller.java
+++ Code Sharing Platform/task/src/platform/controller.java
@@ -1,11 +1,10 @@
 package platform;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ConcurrentModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,7 +12,7 @@ import org.springframework.ui.Model;
 
 @Controller
 public class controller {
-    static final String code = "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
+    static String code = "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
 
     @GetMapping(value = "/code", produces = "text/html")
     public String returnCode(Model model){
@@ -22,10 +21,22 @@ public class controller {
         return "code";
     }
 
+    @GetMapping(value = "/code/new", produces = "text/html")
+    public String newCode(){
+        return "newCode";
+    }
+
     @GetMapping(value = "/api/code",produces = "application/json")
     @ResponseBody
     public ApiCode returnJson(){
         return new ApiCode(code, getTime());
+    }
+
+    @PostMapping(value = "/api/code/new", produces = "application/json")
+    @ResponseBody
+    public String updateSnippet(@RequestBody String code){
+        controller.code = parseJson(code); //parse Json using Gson here!!!!!
+        return "{}";
     }
 
     private String getTime(){
@@ -33,5 +44,10 @@ public class controller {
         LocalDateTime now = LocalDateTime.now();
         String date = dtf.format(now);
         return date;
+    }
+
+    private String parseJson(String Json){
+        JsonObject jObj = new JsonParser().parse(Json).getAsJsonObject();
+        return jObj.get("code").getAsString();
     }
 }
