7410c23759b392af65cf20730822ed7b5b4bc5e4
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2021-07-10 21:09:15 +0800

    fixed return html

--- Code Sharing Platform/task/src/platform/controller.java
+++ Code Sharing Platform/task/src/platform/controller.java
@@ -1,18 +1,24 @@
 package platform;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ConcurrentModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.springframework.ui.Model;
 
 @Controller
 public class controller {
-    static String code = "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
+    static final String code = "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
 
     @GetMapping(value = "/code", produces = "text/html")
-    public String returnCode(){
+    public String returnCode(Model model){
+        model.addAttribute("code", controller.code);
+        model.addAttribute("date", getTime());
         return "code";
     }
 
