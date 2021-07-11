bb0e2e94b72a4b881aa469ece676cd3144111668
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2021-07-11 08:24:27 +0800

    3/5

--- Code Sharing Platform/task/src/platform/ApiCode.java
+++ Code Sharing Platform/task/src/platform/ApiCode.java
@@ -4,10 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
-@AllArgsConstructor
+//not using lombok cause of test
 public class ApiCode {
     private String code;
     private String date;
+    public ApiCode(String code, String date){
+        this.code = code;
+        this.date = date;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- Code Sharing Platform/task/src/platform/controller.java
+++ Code Sharing Platform/task/src/platform/controller.java
@@ -1,6 +1,5 @@
 package platform;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.springframework.stereotype.Controller;
